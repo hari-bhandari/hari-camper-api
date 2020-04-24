bdd861a7abeafc5d19570513eb86b16d31a9311b
2012b <2012bhandari.ha@gmail.com>
2020-04-24 08:59:44 +0100

    v4

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,4 +1,5 @@
 const Course=require('../modals/Course');
+const Bootcamp=require('../modals/Bootcamp');
 const asyncHandler=require('../middleware/async')
 const ErrorResponse=require('../utils/errorResponse')
 
@@ -12,12 +13,81 @@ exports.getCourses=asyncHandler(async (req,res,next)=>{
         query=Course.find({bootcamp:req.params.bootcampId})
     }
     else{
-        query=Course.find();
+        query=Course.find().populate({
+            path:'bootcamp',
+            select:'name description'
+
+        });
     }
     const courses= await query;
     res.status(200).json({
-        sucess:true,
+        success:true,
         count:courses.length,
         data:courses
     });
 })
+//@desc Get a single course
+//@route GET /api/v1/course/:id
+//@route GET /api/v1/bootcamps/:bootcampId/courses
+//@access Public
+exports.getCourse=asyncHandler(async (req,res,next)=>{
+    const course=await Course.findById(req.params.id).populate({
+        path:'bootcamp',
+        select:'name description'
+    });
+    if(!course){
+        return next(new ErrorResponse(`No course with the id of ${req.params.id}`),404);
+    }
+    res.status(200).json({
+        success:true,
+        data:course
+    });
+})
+//@desc add a  course
+//@route POST /api/v1/bootcamps/:bootcampId/courses
+//@access private
+exports.addCourse=asyncHandler(async (req,res,next)=>{
+    req.body.bootcamp=req.params.bootcampId;
+    const bootcamp=await Bootcamp.findById(req.params.bootcampId);
+    if(!bootcamp){
+        return next(new ErrorResponse(`No Bootcamp with the id of ${req.params.bootcampId}`),404);
+    }
+    const course= await Course.create(req.body)
+    res.status(200).json({
+        success:true,
+        data:course
+    });
+});
+//@desc Update a  course
+//@route PUT /api/v1/courses/:id
+//@access private
+exports.updateCourse=asyncHandler(async (req,res,next)=>{
+
+    let course= await Course.findById(req.params.id);
+    if(!course){
+        return next(new ErrorResponse(`No Course with the id of ${req.params.id}`),404);
+    };
+    course=await Course.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true
+    })
+    res.status(200).json({
+        success:true,
+        data:course
+    });
+});
+//@desc Delete a  course
+//@route Delete /api/v1/courses/:id
+//@access private
+exports.deleteCourse=asyncHandler(async (req,res,next)=>{
+
+    const course= await Course.findById(req.params.id);
+    if(!course){
+        return next(new ErrorResponse(`No Course with the id of ${req.params.id}`),404);
+    };
+    await course.remove();
+    res.status(200).json({
+        success:true,
+        data:[]
+    });
+});
--- modals/Course.js
+++ modals/Course.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 
 const CourseSchema = new mongoose.Schema({
@@ -35,12 +36,35 @@ const CourseSchema = new mongoose.Schema({
         type: mongoose.Schema.ObjectId,
         ref: 'Bootcamp',
         required: true
-    },
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: true
     }
 });
 
+// Static method to get avg of course tuitions
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: { $avg: '$tuition' }
+            }
+        }
+    ]);
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+CourseSchema.pre('remove',function(){
+    this.constructor.getAverageCost(this.bootcamp)
+})
+CourseSchema.post('save',function(){
+    this.constructor.getAverageCost(this.bootcamp)
+})
 module.exports=mongoose.model('Course',CourseSchema)
