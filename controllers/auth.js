d00ca4c9fdaf515f33d0d2f6bdd2c45e53e02f53
2012b <2012bhandari.ha@gmail.com>
2020-04-24 19:40:05 +0100

    v7.2

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,17 @@ const ErrorResponse=require('../utils/errorResponse');
 //@route GET /api/v1/auth/register
 //@access Public
 exports.register=asyncHandler(async (req,res,next)=>{
+    const {name,email,password,role}=req.body;
+
+    //Create user
+    const user=await User.create({
+        name,
+        email,
+        password,
+        role
+    });
     res.status(200).json({
         success:true
     })
+
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const bcrypt=require('bcryptjs')
 const UserSchema=new mongoose.Schema({
     name:{
         type:String,
@@ -36,4 +37,9 @@ const UserSchema=new mongoose.Schema({
 
 
 });
+UserSchema.pre('save',async function(next) {
+    const salt=await bcrypt.genSalt(10);
+    this.password=await bcrypt.hash(this.password,salt)
+})
+//Encry password using bcrypt
 module.exports=mongoose.model('User',UserSchema)
