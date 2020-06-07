b6658156f01848e80e4814c7de833bced2dfe2c8
haribhandari000 <61126375+haribhandari000@users.noreply.github.com>
2020-06-07 09:59:27 +0100

    Update bootcamps.js

--- Routes/bootcamps.js
+++ Routes/bootcamps.js
@@ -14,7 +14,7 @@ router.use('/:bootcampId/courses',courseRouter)
 router.use('/:bootcampId/reviews',reviewRouter)
 //env vars
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-router.route('/').get(advancedResults(Bootcamp,'courses'),getBootcamps).post(protect,authorize('publisher','admin'),createBootcamp);
+router.route('/').get(advancedResults(Bootcamp),getBootcamps).post(protect,authorize('publisher','admin'),createBootcamp);
 router.route('/:id').get(getBootcamp).put(protect,authorize('publisher','admin'),updateBootcamp).delete(protect,authorize('publisher','admin'),deleteBootcamp);
 router.route('/:id/photo').put(protect,authorize('publisher','admin'),bootcampPhotoUpload);
 
